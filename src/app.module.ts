bb689203715120bdcaed13022cefc95ecb9f9ca2
d-kens <omondikens1@gmail.com>
2024-06-14 12:38:11 +0300

    typeorm relations

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Employee } from './entities/employee.entity';
+import { ContactInfo } from './entities/contact-infor.entity';
+import { Task } from './entities/task.entity';
+import { Meeting } from './entities/meeting.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'typeorm_relations',
+      entities: [Employee, ContactInfo, Task, Meeting],
+      synchronize: true
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
